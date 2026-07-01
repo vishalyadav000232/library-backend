83bb5b0710b4f8e005e4e4729463972baba1df55
VishaL Yadav <vishalyadav000232@gmail.com>
2026-07-01 15:44:30 +0530

    fix: correct formatting of CORS middleware configuration in create_app function

--- app/main.py
+++ app/main.py
@@ -72,15 +72,17 @@ def create_app() -> FastAPI:
     register_exception_handlers(app)
 
     app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[
-            "http://127.0.0.1:5153",
-            "http://localhost:5153",
-        ],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5173",
+        "http://localhost:5173",
+        "http://127.0.0.1:5153",
+        "http://localhost:5153",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
     app.add_middleware(TokenMiddleware)
 
